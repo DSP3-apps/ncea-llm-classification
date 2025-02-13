a567c3e69a57bb810ee7a915554858b1822d7de1
Andrew <andrew.fergusson@telespazio.com>
2025-02-13 14:20:32 +0000

    Add new JSON input files and update classification flow with ontology collections

--- src/thematic-llm-classification/collate_predictions.py
+++ src/thematic-llm-classification/collate_predictions.py
@@ -1,4 +1,6 @@
 
+import json
+
 from promptflow import tool
 
 
@@ -6,5 +8,17 @@ from promptflow import tool
 # Adding type to arguments and return value will help the system show the types properly
 # Please update the function name/signature per need
 @tool
-def my_python_tool(asset: dict, benefit: dict, pressure: dict, valuation: dict) -> str:
-    return 'hello'
+def my_python_tool(asset: str, benefit: str, pressure: str, valuation: str) -> str:
+    asset_dict = json.loads(asset)
+    benefit_dict = json.loads(benefit)
+    pressure_dict = json.loads(pressure)
+    valuation_dict = json.loads(valuation)
+    
+    merged_doc = {
+        "asset": asset_dict,
+        "benefit": benefit_dict,
+        "pressure": pressure_dict,
+        "valuation": valuation_dict
+    }
+    
+    return json.dumps(merged_doc)
